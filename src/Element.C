a3d677092f35e7c28c521dc8ec3eea6859297e75
Adam Baumgartner <adambaumgartner@gmail.com>
2026-02-16 17:43:18 -0500

    Composite Implementation that Builds

--- src/Element.C
+++ src/Element.C
@@ -2,7 +2,9 @@
 #include "Attr.H"
 #include "Document.H"
 
-Element_Impl::Element_Impl(const std::string & tagName, dom::Document * document) : Node_Impl(tagName, dom::Node::ELEMENT_NODE),
+Element_Impl::Element_Impl(const std::string & tagName, dom::Document * document) : 
+	Node_Impl(tagName, dom::Node::ELEMENT_NODE), 
+	CompositeNode_Impl(tagName, dom::Node::ELEMENT_NODE),
   attributes(document)
 {
 	Node_Impl::document	= document;
--- src/Node.C
+++ src/Node.C
@@ -35,121 +35,17 @@ dom::Node *		Node_Impl::getParentNode(void)
 
 dom::NodeList *		Node_Impl::getChildNodes(void)
 {
-	return &nodes;
+	return &emptyNodes;
 }
 
 dom::Node *		Node_Impl::getFirstChild(void)
 {
-	return *nodes.begin();
+	return 0;
 }
 
 dom::Node *		Node_Impl::getLastChild(void)
 {
-	return *(--nodes.end());
-}
-
-dom::Node *		Node_Impl::getPreviousSibling(void)
-{
-	return getSibling(-1);
-}
-
-dom::Node *		Node_Impl::getNextSibling(void)
-{
-	return getSibling(1);
-}
-
-dom::Document *		Node_Impl::getOwnerDocument(void)
-{
-	return document;
-}
-
-dom::Node *		Node_Impl::insertBefore(dom::Node * newChild, dom::Node * refChild)
-{
-	if (newChild->getOwnerDocument() != getOwnerDocument())
-		throw dom::DOMException(dom::DOMException::WRONG_DOCUMENT_ERR, "New Child is not a part of this document.");
-
-	if (newChild->getParentNode() != 0)
-		newChild->getParentNode()->removeChild(newChild);
-
-	if (refChild == 0)
-	{
-		nodes.push_back(newChild);
-		(dynamic_cast<Node_Impl *>(newChild))->setParent(this);
-		return newChild;
-	}
-
-	dom::NodeList::iterator	index	= nodes.find(refChild);
-
-	if (index == nodes.end())
-		throw dom::DOMException(dom::DOMException::NOT_FOUND_ERR, "Reference Child is not a child of this node.");
-
-	nodes.insert(++index, newChild);
-	(dynamic_cast<Node_Impl *>(newChild))->setParent(this);
-
-	return newChild;
-}
-
-dom::Node *		Node_Impl::replaceChild(dom::Node * newChild, dom::Node * oldChild)
-{
-	if (newChild->getOwnerDocument() != getOwnerDocument())
-		throw dom::DOMException(dom::DOMException::WRONG_DOCUMENT_ERR, "New Child is not a part of this document.");
-
-	if (newChild->getParentNode() != 0)
-		newChild->getParentNode()->removeChild(newChild);
-
-	dom::NodeList::iterator	index	= nodes.find(oldChild);
-
-	if (index == nodes.end())
-		throw dom::DOMException(dom::DOMException::NOT_FOUND_ERR, "Old Child is not a child of this node.");
-
-	nodes.insert(index, newChild);
-	(dynamic_cast<Node_Impl *>(newChild))->setParent(this);
-	(dynamic_cast<Node_Impl *>(*index))->setParent(0);
-	nodes.erase(index);
-
-	return oldChild;
-}
-
-dom::Node *		Node_Impl::removeChild(dom::Node * oldChild)
-{
-	dom::NodeList::iterator	index	= nodes.find(oldChild);
-
-	if (index == nodes.end())
-		throw dom::DOMException(dom::DOMException::NOT_FOUND_ERR, "Old Child is not a child of this node.");
-
-	(dynamic_cast<Node_Impl *>(*index))->setParent(0);
-	nodes.erase(index);
-
-	return oldChild;
-}
-
-dom::Node *		Node_Impl::appendChild(dom::Node * newChild)
-{
-	if (newChild->getOwnerDocument() != getOwnerDocument())
-		throw dom::DOMException(dom::DOMException::WRONG_DOCUMENT_ERR, "New Child is not a part of this document.");
-
-	if (newChild->getParentNode() != 0)
-		newChild->getParentNode()->removeChild(newChild);
-
-	nodes.push_back(newChild);
-	(dynamic_cast<Node_Impl *>(newChild))->setParent(this);
-
-	return newChild;
-}
-
-bool			Node_Impl::hasChildNodes(void)
-{
-	return nodes.size() > 0;
-}
-
-const std::string &	Node_Impl::getLocalName(void)
-{
-	return name;
-}
-
-void Node_Impl::setParent(dom::Node * parent)
-{
-	this->parent	= parent;
+	return 0;
 }
 
 dom::Node *		Node_Impl::getSibling(int direction)
@@ -175,4 +71,183 @@ dom::Node *		Node_Impl::getSibling(int direction)
 		else
 			return *i;
 	}
+}
+
+dom::Node *		Node_Impl::getPreviousSibling(void)
+{
+	return getSibling(-1);
+}
+
+dom::Node *		Node_Impl::getNextSibling(void)
+{
+	return getSibling(1);
+}
+
+dom::Document *		Node_Impl::getOwnerDocument(void)
+{
+	return document;
+}
+
+dom::Node *		Node_Impl::insertBefore(dom::Node * newChild, dom::Node * refChild)
+{
+	// (void)newChild;
+	// (void)refChild;
+	throw dom::DOMException(
+		dom::DOMException::HIERARCHY_REQUEST_ERR, 
+			"This node cannot have children.");
+}
+
+dom::Node *		Node_Impl::replaceChild(dom::Node * newChild, dom::Node * oldChild)
+{
+	// (void)newChild;
+	// (void)refChild;
+	throw dom::DOMException(
+		dom::DOMException::HIERARCHY_REQUEST_ERR, 
+			"This node cannot have children.");
+}
+
+dom::Node *		Node_Impl::removeChild(dom::Node * oldChild)
+{
+	// (void)newChild;
+	// (void)refChild;
+	throw dom::DOMException(
+		dom::DOMException::HIERARCHY_REQUEST_ERR, 
+			"This node cannot have children.");
+}
+
+dom::Node *		Node_Impl::appendChild(dom::Node * newChild)
+{
+	// (void)newChild;
+	// (void)refChild;
+	throw dom::DOMException(
+		dom::DOMException::HIERARCHY_REQUEST_ERR, 
+			"This node cannot have children.");
+}
+
+bool			Node_Impl::hasChildNodes(void)
+{
+	return false;
+}
+
+const std::string &	Node_Impl::getLocalName(void)
+{
+	return name;
+}
+
+void Node_Impl::setParent(dom::Node * parent)
+{
+	this->parent	= parent;
+}
+
+CompositeNode_Impl::CompositeNode_Impl(const std::string & n, short type) :
+	Node_Impl(n, type)
+{
+}
+
+CompositeNode_Impl::~CompositeNode_Impl()
+{
+}
+
+dom::NodeList *	CompositeNode_Impl::getChildNodes(void)
+{
+	return &nodes;
+}
+
+dom::Node *	CompositeNode_Impl::getFirstChild(void)
+{
+	if (nodes.empty())
+		return 0;
+
+	return *nodes.begin();
+}
+
+dom::Node *	CompositeNode_Impl::getLastChild(void)
+{
+	if (nodes.empty())
+		return 0;
+
+	return *(--nodes.end());
+}
+
+dom::Node *	CompositeNode_Impl::insertBefore(dom::Node * newChild,
+	dom::Node * refChild)
+{
+	if (newChild->getOwnerDocument() != getOwnerDocument())
+		throw dom::DOMException(dom::DOMException::WRONG_DOCUMENT_ERR, "New Child is not a part of this document.");
+
+	if (newChild->getParentNode() != 0)
+		newChild->getParentNode()->removeChild(newChild);
+
+	if (refChild == 0)
+	{
+		nodes.push_back(newChild);
+		(dynamic_cast<Node_Impl *>(newChild))->setParent(this);
+		return newChild;
+	}
+
+	dom::NodeList::iterator	index	= nodes.find(refChild);
+
+	if (index == nodes.end())
+		throw dom::DOMException(dom::DOMException::NOT_FOUND_ERR, "Reference Child is not a child of this node.");
+
+	nodes.insert(++index, newChild);
+	(dynamic_cast<Node_Impl *>(newChild))->setParent(this);
+
+	return newChild;
+}
+
+dom::Node *	CompositeNode_Impl::replaceChild(dom::Node * newChild, 
+	dom::Node * oldChild)
+{
+	if (newChild->getOwnerDocument() != getOwnerDocument())
+		throw dom::DOMException(dom::DOMException::WRONG_DOCUMENT_ERR, "New Child is not a part of this document.");
+
+	if (newChild->getParentNode() != 0)
+		newChild->getParentNode()->removeChild(newChild);
+
+	dom::NodeList::iterator	index	= nodes.find(oldChild);
+
+	if (index == nodes.end())
+		throw dom::DOMException(dom::DOMException::NOT_FOUND_ERR, "Old Child is not a child of this node.");
+
+	nodes.insert(index, newChild);
+	(dynamic_cast<Node_Impl *>(newChild))->setParent(this);
+	(dynamic_cast<Node_Impl *>(*index))->setParent(0);
+	nodes.erase(index);
+
+	return oldChild;
+}
+
+dom::Node *	CompositeNode_Impl::removeChild(dom::Node * oldChild)
+{
+	dom::NodeList::iterator	index	= nodes.find(oldChild);
+
+	if (index == nodes.end())
+		throw dom::DOMException(dom::DOMException::NOT_FOUND_ERR, "Old Child is not a child of this node.");
+
+	(dynamic_cast<Node_Impl *>(*index))->setParent(0);
+	nodes.erase(index);
+
+	return oldChild;
+}
+
+dom::Node *	CompositeNode_Impl::appendChild(dom::Node * newChild)
+{
+	if (newChild->getOwnerDocument() != getOwnerDocument())
+		throw dom::DOMException(
+			dom::DOMException::WRONG_DOCUMENT_ERR, 
+			"New Child is not a part of this document.");
+
+	if (newChild->getParentNode() != 0)
+		newChild->getParentNode()->removeChild(newChild);
+
+	nodes.push_back(newChild);
+	(dynamic_cast<Node_Impl *>(newChild))->setParent(this);
+
+	return newChild;
+}
+
+bool CompositeNode_Impl::hasChildNodes(void)
+{
+	return nodes.size() > 0;
 }
