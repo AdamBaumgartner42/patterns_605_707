7c812585aacea0a4ff1fd523cd28865aee1ed791
adambaumgartner42 <adambaumgartner@gmail.com>
2026-03-20 08:25:42 -0400

    3 functions added

--- src/NodeAdapter.C
+++ src/NodeAdapter.C
@@ -29,3 +29,227 @@ const XERCES::XMLCh* NodeAdapter_JHU_EP::getNodeName() const
     // Convert std::string to XMLch*, which is a char *
     return (const XERCES::XMLCh*) stringBuffer.c_str();
 }
+
+const XERCES::XMLCh* NodeAdapter_JHU_EP::getNodeValue() const
+{
+    // Using the same method as getNodeName
+    stringBuffer = impl_JHU_EP->getNodeValue();
+    return (const XERCES::XMLCh*) stringBuffer.c_str();
+}
+
+XERCES::DOMNode::NodeType NodeAdapter_JHU_EP::getNodeType() const
+{
+    // switch statement using getNodeType
+    switch(impl_JHU_EP->getNodeType()) {
+        case ATTRIBUTE_NODE :
+            return XERCES::DOMNode::NodeType::ATTRIBUTE_NODE;
+        case DOCUMENT_NODE :
+            return XERCES::DOMNode::NodeType::DOCUMENT_NODE;
+        case ELEMENT_NODE :
+            return XERCES::DOMNode::NodeType::ELEMENT_NODE;
+        case TEXT_NODE :
+            return XERCES::DOMNode::NodeType::TEXT_NODE;
+        default:
+            return XERCES::DOMNode::NodeType::ELEMENT_NODE;
+            // Element is generic so a good choice for fall through
+    }
+}
+
+XERCES::DOMNode* NodeAdapter_JHU_EP::getParentNode() const
+{
+    return nullptr;
+}
+
+XERCES::DOMNodeList* NodeAdapter_JHU_EP::getChildNodes() const
+{
+    return nullptr;
+}
+
+XERCES::DOMNode* NodeAdapter_JHU_EP::getFirstChild() const
+{
+    return nullptr;
+}
+
+XERCES::DOMNode* NodeAdapter_JHU_EP::getLastChild() const
+{
+    return nullptr;
+}
+
+XERCES::DOMNode* NodeAdapter_JHU_EP::getPreviousSibling() const
+{
+    return nullptr;
+}
+
+XERCES::DOMNode* NodeAdapter_JHU_EP::getNextSibling() const
+{
+    return nullptr;
+}
+
+XERCES::DOMNamedNodeMap* NodeAdapter_JHU_EP::getAttributes() const
+{
+    return nullptr;
+}
+
+XERCES::DOMDocument* NodeAdapter_JHU_EP::getOwnerDocument() const
+{
+    return nullptr;
+}
+
+XERCES::DOMNode* NodeAdapter_JHU_EP::cloneNode(bool deep) const
+{
+    (void)deep;
+    return nullptr;
+}
+
+XERCES::DOMNode* NodeAdapter_JHU_EP::insertBefore(XERCES::DOMNode* newChild, XERCES::DOMNode* refChild)
+{
+    (void)newChild;
+    (void)refChild;
+    return nullptr;
+}
+
+XERCES::DOMNode* NodeAdapter_JHU_EP::replaceChild(XERCES::DOMNode* newChild, XERCES::DOMNode* oldChild)
+{
+    (void)newChild;
+    (void)oldChild;
+    return nullptr;
+}
+
+XERCES::DOMNode* NodeAdapter_JHU_EP::removeChild(XERCES::DOMNode* oldChild)
+{
+    (void)oldChild;
+    return nullptr;
+}
+
+XERCES::DOMNode* NodeAdapter_JHU_EP::appendChild(XERCES::DOMNode* newChild)
+{
+    (void)newChild;
+    return nullptr;
+}
+
+bool NodeAdapter_JHU_EP::hasChildNodes() const
+{
+    return false;
+}
+
+void NodeAdapter_JHU_EP::setNodeValue(const XERCES::XMLCh* nodeValue)
+{
+    (void)nodeValue;
+}
+
+// Level 2
+
+void NodeAdapter_JHU_EP::normalize()
+{
+}
+
+bool NodeAdapter_JHU_EP::isSupported(const XERCES::XMLCh* feature, const XERCES::XMLCh* version) const
+{
+    (void)feature;
+    (void)version;
+    return false;
+}
+
+const XERCES::XMLCh* NodeAdapter_JHU_EP::getNamespaceURI() const
+{
+    return nullptr;
+}
+
+const XERCES::XMLCh* NodeAdapter_JHU_EP::getPrefix() const
+{
+    return nullptr;
+}
+
+const XERCES::XMLCh* NodeAdapter_JHU_EP::getLocalName() const
+{
+    return nullptr;
+}
+
+void NodeAdapter_JHU_EP::setPrefix(const XERCES::XMLCh* prefix)
+{
+    (void)prefix;
+}
+
+bool NodeAdapter_JHU_EP::hasAttributes() const
+{
+    return false;
+}
+
+// Level 3
+
+bool NodeAdapter_JHU_EP::isSameNode(const XERCES::DOMNode* other) const
+{
+    return this == other;
+}
+
+bool NodeAdapter_JHU_EP::isEqualNode(const XERCES::DOMNode* arg) const
+{
+    (void)arg;
+    return false;
+}
+
+void* NodeAdapter_JHU_EP::setUserData(const XERCES::XMLCh* key, void* data, XERCES::DOMUserDataHandler* handler)
+{
+    (void)key;
+    (void)handler;
+    return data;
+}
+
+void* NodeAdapter_JHU_EP::getUserData(const XERCES::XMLCh* key) const
+{
+    (void)key;
+    return nullptr;
+}
+
+const XERCES::XMLCh* NodeAdapter_JHU_EP::getBaseURI() const
+{
+    return nullptr;
+}
+
+short NodeAdapter_JHU_EP::compareDocumentPosition(const XERCES::DOMNode* other) const
+{
+    (void)other;
+    return 0;
+}
+
+const XERCES::XMLCh* NodeAdapter_JHU_EP::getTextContent() const
+{
+    return nullptr;
+}
+
+void NodeAdapter_JHU_EP::setTextContent(const XERCES::XMLCh* textContent)
+{
+    (void)textContent;
+}
+
+const XERCES::XMLCh* NodeAdapter_JHU_EP::lookupPrefix(const XERCES::XMLCh* namespaceURI) const
+{
+    (void)namespaceURI;
+    return nullptr;
+}
+
+bool NodeAdapter_JHU_EP::isDefaultNamespace(const XERCES::XMLCh* namespaceURI) const
+{
+    (void)namespaceURI;
+    return false;
+}
+
+const XERCES::XMLCh* NodeAdapter_JHU_EP::lookupNamespaceURI(const XERCES::XMLCh* prefix) const
+{
+    (void)prefix;
+    return nullptr;
+}
+
+void* NodeAdapter_JHU_EP::getFeature(const XERCES::XMLCh* feature, const XERCES::XMLCh* version) const
+{
+    (void)feature;
+    (void)version;
+    return nullptr;
+}
+
+// Non-standard
+
+void NodeAdapter_JHU_EP::release()
+{
+    delete this;
+}
