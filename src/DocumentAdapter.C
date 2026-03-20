dff4ec4860f3c765f48c85838ffd28611f5fa2c6
adambaumgartner42 <adambaumgartner@gmail.com>
2026-03-20 16:27:08 -0400

    document dom

--- src/DocumentAdapter.C
+++ src/DocumentAdapter.C
@@ -4,4 +4,24 @@
 #include "DocumentAdapter.H"
 
 
-DocumentAdapter_JHU_EP :: DocumentAdapter_JHU_EP(Document_Impl* n) : impl_JHU_EP (n) {}
+DocumentAdapter_JHU_EP::DocumentAdapter_JHU_EP(Document_Impl* n) : impl_JHU_EP (n) {}
+
+DocumentAdapter_JHU_EP::DOMElement* createElement(const XMLCh *tagName)
+{
+    return nullptr;
+}
+
+DocumentAdapter_JHU_EP::DOMText* createTextNode(const XMLCh *data)
+{
+    return nullptr;
+}
+
+DocumentAdapter_JHU_EP::DOMAttr* createAttribute(const XMLCh *name)
+{
+    return nullptr;
+}
+
+DocumentAdapter_JHU_EP::DOMElement* getDocumentElement() const
+{
+    return nullptr;
+}
