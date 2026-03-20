4f756c913b86321f70c26158a51ea114f29cdcbc
adambaumgartner42 <adambaumgartner@gmail.com>
2026-03-20 19:20:13 -0400

    wrappers

--- src/DocumentAdapter.C
+++ src/DocumentAdapter.C
@@ -2,33 +2,44 @@
  * @brief Adapter for JHU_EP documents to be read as w3c documents
  */
 #include "DocumentAdapter.H"
-#include "Element.H"
+
+#include <string>
+
+// --- Needed but not present ---
+// #include "DOMElement"
+// #include "DOMText"
+// #include "DOMAttr"
 
 
+// Cpnstructors
 DocumentAdapter_JHU_EP::DocumentAdapter_JHU_EP(Document_Impl* n) : doc_impl_JHU_EP (n) {}
-
-
+ElementWrapper_JHU_EP::ElementWrapper_JHU_EP(dom::Element* e) : element_JHU_EP (e) {}
+TextWrapper_JHU_EP::TextWrapper_JHU_EP(dom::Text* t) : text_JHU_EP (t) {}
+AttrWrapper_JHU_EP::AttrWrapper_JHU_EP(dom::Attr* t) : attr_JHU_EP (t) {}
 
 XERCES::DOMElement* DocumentAdapter_JHU_EP::createElement(const XERCES::XMLCh *tagName)
 {
-    // Pass the value to our composed class of Document_Impl
-    doc_impl_JHU_EP->createElement(tagName);
-    
-    
-    return nullptr;
+    // Pass the value to our composed class of Document_Impl 
+    dom::Element* createdElement = doc_impl_JHU_EP->createElement(std::string(tagName));
+
+    // Wrap value for XERCES::DOMElement return type
+    return new ElementWrapper_JHU_EP(createdElement);
 }
 
 XERCES::DOMText* DocumentAdapter_JHU_EP::createTextNode(const XERCES::XMLCh *data)
-{
-    return nullptr;
+{   
+    dom::Text* createdText = doc_impl_JHU_EP->createTextNode(std::string(data));
+    return new TextWrapper_JHU_EP(createdText);
 }
 
 XERCES::DOMAttr* DocumentAdapter_JHU_EP::createAttribute(const XERCES::XMLCh *name)
-{
-    return nullptr;
+{   
+    dom::Attr* createdAttribute = doc_impl_JHU_EP->createAttribute(std::string(name));
+    return new AttrWrapper_JHU_EP(createdAttribute);
 }
 
 XERCES::DOMElement* DocumentAdapter_JHU_EP::getDocumentElement() const
 {
-    return nullptr;
+    createdElement = doc_impl_JHU_EP->getDocumentElement();
+    return new ElementWrapper_JHU_EP(createdElement);
 }
