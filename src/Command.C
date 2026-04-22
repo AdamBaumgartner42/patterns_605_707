1218c85538d47a3251d40eb8cebed68db8e82785
adambaumgartner42 <adambaumgartner@gmail.com>
2026-04-22 04:38:29 -0400

    remove duplicate code

--- src/Command.C
+++ src/Command.C
@@ -1,50 +1,8 @@
+#include <iostream>
 #include "Command.H"
 
+
 void RunSerial::Execute() const {
-    //
-	// Create tree of this document:
-	// <? xml version="1.0" encoding="UTF-8"?>
-	// <document>
-	//   <element attribute="attribute value"/>
-	//   <element/>
-	//   <element attribute="attribute value" attribute2="attribute2 value">
-	//     Element Value
-	//   </element>
-	//   <element>
-	//   </element>
-	// </document>
-	//
-	dom::Document *	document	= new Document_Impl;
-	dom::Element *	root		= document->createElement("document");
-	document->appendChild(root);
 
-	dom::Element *	child		= document->createElement("element");
-	dom::Attr *	attr		= document->createAttribute("attribute");
-	attr->setValue("attribute value");
-	child->setAttributeNode(attr);
-	root->appendChild(child);
-
-	child				= document->createElement("element");
-	root->appendChild(child);
-
-	child				= document->createElement("element");
-	child->setAttribute("attribute", "attribute value");
-	child->setAttribute("attribute2", "attribute2 value");
-	dom::Text *	text		= document->createTextNode("Element Value");
-	child->appendChild(text);
-	root->appendChild(child);
-
-	child				= document->createElement("element");
-	root->appendChild(child);
-
-	//
-	// Serialize
-	//
-	XMLSerializer	xmlSerializer(argv[2]);
-	xmlSerializer.serializePretty(document);
-	XMLSerializer	xmlSerializer2(argv[3]);
-	xmlSerializer2.serializeMinimal(document);
-
-	// delete Document and tree.
-
+    std::cout << "Hello World\n" ;
 }
